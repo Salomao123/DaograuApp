f3f345cea56dfc67b921a3a8c5c70aa7a8291b45
José Luías <zeluis.ap@gmail.com>
2019-11-13 10:47:43 -0300

    aplicando regra de ecibição de tela

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import './config/ReactotronConfig';
 
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import Routes from './routes';
+import createNavigator from './routes';
 
 import {Provider} from 'react-redux';
 import store from './store';
 
 import {setNavigator} from './services/navigation';
 
-export default class src extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Routes ref={setNavigator} />
-      </Provider>
-    );
+function App() {
+  const [userChecked, setUserChecked] = useState(false);
+  const [userLogged, setUserLogged] = useState(false);
+
+  useEffect(() => {
+    validateAuth();
+  });
+
+  async function validateAuth() {
+    const token_user = await AsyncStorage.getItem('@User_token');
+
+    setUserChecked(true);
+    setUserLogged(!!token_user);
   }
+
+  if (!userChecked) return null;
+
+  const Routes = createNavigator(userLogged);
+
+  return (
+    <Provider store={store}>
+      <Routes ref={setNavigator} />
+    </Provider>
+  );
 }
+
+export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,23 @@
-import React from 'react';
-
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
-const AppNavigator = createAppContainer(
-  createSwitchNavigator({
-    Login: {
-      screen: Login,
-    },
-    Dashboard: {
-      screen: Dashboard,
-    },
-  }),
-);
+const AppNavigator = (userLogged = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Login: {
+          screen: Login,
+        },
+        Dashboard: {
+          screen: Dashboard,
+        },
+      },
+      {
+        initialRouteName: userLogged ? 'Dashboard' : 'Login',
+      },
+    ),
+  );
 
 export default AppNavigator;
